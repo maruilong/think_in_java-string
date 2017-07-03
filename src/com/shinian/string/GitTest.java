ad514cdf11dd90eae6b4b893df1c9b6b172d3e50
maruilong <774328384@qq.com>
2017-07-03 09:44:27 +0800

    删除测试类

--- src/com/shinian/string/GitTest.java
+++
@@ -1,8 +0,0 @@
-package com.shinian.string;
-
-public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("helloWorld");
-        System.out.println(1);
-    }
-}
